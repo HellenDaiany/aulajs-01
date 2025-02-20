baa6724e03de0659ef211d4f5711ad125bad9406
Hellen Daiany <hellendaiiany@gmail.com>
2025-02-20 18:02:34 -0300

    variaveis

--- scripts.js
+++ scripts.js
@@ -1,2 +1,45 @@
-console.log ("Hello Word!");
-console.log ("valindando js conectado ao HTML");
+
+
+//Declarando uma variável sem valor.
+ var user;
+//para exibir o conteudo
+console.log(user);
+
+//Declarando uma variável com valor.
+ var email = "hellen@gmail.com";
+ /*Neste caso, estou declarando uma variável chamada "email"
+ e atribuindo a ela (através do sinal de igual "=") o valor inicial(hellen@gmail.com)*/
+
+ //exibindo...
+ console.log(email);
+
+// substituindo o valor da variável.
+ email = "daiany@gmail.com";
+ //exibindo...
+ console.log(email);
+
+ //Outra maneira de criar variável é usando o "let"
+ //Declarando uma variável sem valor com let;
+
+ let User
+ //exibindo...
+ console.log("Dai");
+
+ //Declarando uma variável com valor.
+ let Email = "hellen@gmail.com";
+ /*Neste caso, estou declarando uma variável let chamada "email"
+ e atribuindo a ela (através do sinal de igual "=") o valor inicial(hellen@gmail.com)*/
+
+ //exibindo...
+ console.log(Email);
+
+// substituindo o valor da variável.
+ Email = "daiany@gmail.com";
+ //exibindo...
+ console.log(Email);
+
+ // EXiste também o const, que se trata de uma variável onde o valor é fixo, ou seja, nao pode ser alterado.
+
+ const nuber = 42;
+//exibindo...
+console.log (nuber);
